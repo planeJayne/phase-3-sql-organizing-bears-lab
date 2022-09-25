d5cfedf730df75ac6d495a7083b6f1d62937c40d
Jasmin <jasmin.parish@outlook.com>
2022-09-25 01:24:23 -0400

    commit tree a1826921f7cc56dcaf07f7e1d3c84256f2e23268

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Mr. Chocolate', 7, 'Male', 'Brown', 'Neutral');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Rowdy', 1, 'Male', 'Black', 'Playful');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Tabitha', 22, 'Female', 'Gray', 'Social');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Sergeant Brown', 17, 'Male', 'White', 'Choleric');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Melissa', 4, 'Female', 'Black', 'Relaxed');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Grinch', 47, 'Male', 'Brown', 'Analytical');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES ('Wendy', 9, 'Female', 'Black', 'Social');
+INSERT INTO bears (name, age, sex, color, temperament)
+VALUES (null, 12, 'Female', 'White', 'Relaxed');
